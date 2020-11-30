47131d54c4e36eb931b6d618963bccc40fca090b
pereira-bruno <brunopereiradm@outlook.com>
2020-11-30 14:20:03 -0300

    Classe disciplina alterada

--- src/java/br/fatecpg/poo/Disciplina.java
+++ src/java/br/fatecpg/poo/Disciplina.java
@@ -5,63 +5,154 @@
  */
 package br.fatecpg.poo;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.ArrayList;
+import web.DbListener;
 
 /**
  *
  * @author bruno
  */
 public class Disciplina {
+    long rowId;
     String nome, ementa;
-    int ciclo, nota;
+    int ciclo;
+    double nota;
 
-    public Disciplina(String nome, String ementa, int ciclo) {
+    public Disciplina(long rowId, String nome, String ementa, int ciclo, double nota) {
+        this.rowId = rowId;
         this.nome = nome;
         this.ementa = ementa;
         this.ciclo = ciclo;
+        this.nota = nota;
     }
-
-    private static ArrayList<Disciplina> disciplinas;
     
-    public static ArrayList<Disciplina> getList(){
-        if(disciplinas == null){
-            disciplinas = new ArrayList<>();
-            disciplinas.add(new Disciplina("Gestão de Projetos", "Definição de projeto segundo concepção difundida pelas melho"
-                    + "res práticas de gestão de projetos. Histórico do desenvolvimento do conjunto de conhecimentos de gest"
-                    + "ão de projetos. Comparação ente o gerenciamento por projetos com o gerenciamento tradicional. O cicl"
-                    + "o de vida de um projeto. Os fatores de sucesso e insucesso de projetos e sua mensuração. As nove de c"
-                    + "onhecimento para a gestão de projetos e seus processos : Integração, Escopo, Tempo, Custo, Qualidade, R"
-                    + "ecursos Humanos, Comunicações, Riscos e Aquisições.", 5));
-            disciplinas.add(new Disciplina("Laboratório de Banco de Dados", "Tecnologias emergentes de mercado que serão apl"
-                    + "icadas em laboratório", 5));
-            disciplinas.add(new Disciplina("Laboratório de Engenharia de Software", "Desenvolvimento de um software utilizan"
-                    + "do os conhecimentos adquiridos ao longo do curso. A elaboração deve abordar as disciplinas de requ"
-                    + "isitos, análise e projeto, implementação, implantação e gerência de projetos. O processo de desenvol"
-                    + "vimento, assim como a técnica fica a critério de acordo entre professor e aluno.", 5));
-            disciplinas.add(new Disciplina("Programação para Dispositivos Móveis", "Ambientes de programação para dispositivos "
-                    + "móveis. Emuladores. Interface gráfica, serviços baseados em localização, armazenamento de dados persist"
-                    + "entes, serviços de telefonia e comunicação entre processos. Desenvolvimento de aplicações com J2ME.", 5));
-            disciplinas.add(new Disciplina("Redes de Computadores", "Comunicação de Dados. Topologia e Características Físic"
-                    + "as de Redes. Redes Locais de Longa Distância. Redes de Alta Velocidade. Protocolos e Serviços de Comunic"
-                    + "ação. Camadas de Sistemas Abertos. Sistemas Operacionais de Redes. Interconexão de redes. Avaliação de "
-                    + "Desempenho. Estrutura e Funcionamento da Internet.", 5));
-            disciplinas.add(new Disciplina("Segurança da Informação", "Requisitos de segurança de aplicações, de base de dad"
-                    + "os e de comunicações. Segurança de dispositivos móveis. Políticas de segurança. Criptografia. Firewa"
-                    + "lls. Vulnerabilidades e principais tecnologias de segurança.", 5));
-            disciplinas.add(new Disciplina("Inglês V", "Aprofundamento da compreensão e produção oral e escrita por meio funç"
-                    + "ões sociais e estruturas mais complexas da língua. Ênfase na oralidade, atendendo às especificidades "
-                    + "acadêmico-profissionais da área e abordando aspectos sócio-culturais da língua inglesa.", 5));
-            disciplinas.add(new Disciplina("Trabalho de Graduação I", "O estudante elaborará, sob a orientação de docente, u"
-                    + "m Trabalho de Graduação, e o apresentará perante uma banca examinadora. As disciplinas de Projetos d"
-                    + "everão subsidiar o trabalho de graduação, com temas e propostas de projetos, casos, etc.", 5));
-            disciplinas.add(new Disciplina("Programação Orientada a Objetos", "Conceitos e evolução da tecnologia de orientaç"
-                    + "ão a objetos. Limitações e diferenças entre o paradigma da programação estruturada em relação à orien"
-                    + "tação a objetos. Conceito de objeto, classe, métodos, atributos, herança, polimorfismo, agregação, ass"
-                    + "ociação, dependência, encapsulamento, mensagem e suas respectivas notações na linguagem padrão de repr"
-                    + "esentação da orientação a objetos. Implementação de algoritmos orientado a objetos utilizando linguage"
-                    + "ns de programação. Aplicação e uso das estruturas fundamentais da orientação a objetos.", 4));
+    public static ArrayList<Disciplina> getList() throws Exception{
+        ArrayList<Disciplina> disciplinas = new ArrayList<>();
+        Connection con = null;
+        Statement stmt = null;
+        ResultSet rs = null;
+        Exception methodException = null;
+        try{
+            con = DbListener.getConnection();
+            stmt = con.createStatement();
+            rs = stmt.executeQuery("SELECT rowId, * FROM disciplinas");
+            while(rs.next()){
+                disciplinas.add(new Disciplina(
+                        rs.getLong("rowId"),
+                        rs.getString("nome"),
+                        rs.getString("ementa"),
+                        rs.getInt("ciclo"),
+                        rs.getDouble("nota")
+                ));
+            }
+        }catch(Exception ex){
+            methodException = ex;
+        }finally{
+            try{
+               rs.close();
+           }catch(Exception ex1){};
+            try{
+               stmt.close();
+           }catch(Exception ex1){};
+           try{
+               con.close();
+           }catch(Exception ex1){};
         }
+        
+        if(methodException != null) throw methodException;
+        
         return disciplinas;
+    }
+    
+    public static void insert(String nome, String ementa, int ciclo, double nota) throws Exception{
+        Connection con = null;
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+        Exception methodException = null;
+        try{
+            con = DbListener.getConnection();
+            stmt = con.prepareStatement("INSERT INTO disciplinas VALUES (?, ?, ?, ?)");
+            stmt.setString(1, nome);
+            stmt.setString(2, ementa);
+            stmt.setInt(3, ciclo);
+            stmt.setDouble(4, nota);
+            stmt.execute();
+        }catch(Exception ex){
+            methodException = ex;
+        }finally{
+            try{
+               rs.close();
+           }catch(Exception ex1){};
+            try{
+               stmt.close();
+           }catch(Exception ex1){};
+           try{
+               con.close();
+           }catch(Exception ex1){};
+        }
+        if(methodException != null) throw methodException;
+    }
+    
+    
+    public static void update(long rowId, String nome, String ementa, int ciclo, double nota) throws Exception{
+        Connection con = null;
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+        Exception methodException = null;
+        try{
+            con = DbListener.getConnection();
+            stmt = con.prepareStatement("UPDATE disciplinas SET nome=?, ementa=?, ciclo=?, nota=? "
+                    + "WHERE rowId=?");
+            stmt.setString(1, nome);
+            stmt.setString(2, ementa);
+            stmt.setInt(3, ciclo);
+            stmt.setDouble(4, nota);
+            stmt.setLong(5, rowId);
+            stmt.execute();
+        }catch(Exception ex){
+            methodException = ex;
+        }finally{
+            try{
+               rs.close();
+           }catch(Exception ex1){};
+            try{
+               stmt.close();
+           }catch(Exception ex1){};
+           try{
+               con.close();
+           }catch(Exception ex1){};
+        }
+        if(methodException != null) throw methodException;
+    }
+    
+    public static void delete(long rowId) throws Exception{
+        Connection con = null;
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+        Exception methodException = null;
+        try{
+            con = DbListener.getConnection();
+            stmt = con.prepareStatement("DELETE FROM disciplinas WHERE rowId=?");
+            stmt.setLong(1, rowId);
+            stmt.execute();
+        }catch(Exception ex){
+            methodException = ex;
+        }finally{
+            try{
+               rs.close();
+           }catch(Exception ex1){};
+            try{
+               stmt.close();
+           }catch(Exception ex1){};
+           try{
+               con.close();
+           }catch(Exception ex1){};
+        }
+        if(methodException != null) throw methodException;
     }
     
     
@@ -89,16 +180,31 @@ public class Disciplina {
         this.ciclo = ciclo;
     }
 
-    public int getNota() {
+    public double getNota() {
         return nota;
     }
 
     public void setNota(int nota) {
         this.nota = nota;
     }
+
+    public long getRowId() {
+        return rowId;
+    }
+
+    public void setRowId(long rowId) {
+        this.rowId = rowId;
+    }
+    
+    
     
     public static String getCreateStatement() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return "CREATE TABLE IF NOT EXISTS disciplinas("
+                + "nome VARCHAR(50) UNIQUE NOT NULL,"
+                + "ementa VARCHAR(400) NOT NULL,"
+                + "ciclo NUMBER(2) NOT NULL,"
+                + "nota NUMBER(2,2) DEFAULT 0"
+                + ")";
     }
     
 }
